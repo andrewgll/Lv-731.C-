06668af718aa3cc4939a65ced71d36ee9e7c0fc9
andrewgll <dungmajor233@gmail.com>
2022-12-28 21:35:09 +0200

    removed

--- Members/Andrii_Hladkyi/Task_1/scripts/init_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-gcc -Wall -g ./headers/stringList.c ./code/main.c -o ./bin/stringList_debug
-
-./bin/stringList_debug
--- Members/Andrii_Hladkyi/Task_2/scripts/init_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-g++ -g ./lib/AnyType.cpp ./code/main.cpp -o ./bin/anytype
-
-./bin/anytype
