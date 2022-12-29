9ae7a3b22f0e3485a276cfaef73eda276496c8b7
Nazar Drozd <nazardrozd228@gmail.com>
2022-12-29 12:24:13 +0200

    Task1

---
+++ NazarDrozd/Task1/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "MyList.h"
+
+int main()
+{
+	char** node = NULL;
+
+	char str0[10] = "string0";
+	char str1[10] = "string1";
+	char str2[10] = "string2";
+
+	StringListInit(&node);
+	StringListAdd(node, str0);
+	StringListAdd(node, str1);
+	StringListAdd(node, str2);
+
+	std::cout << "List size =  " << StringListSize(node) << "\n\n";
+	ListShow(node);
+
+	StringListReplaceInStrings(node, str1, str2);
+
+	std::cout << "List size =  " << StringListSize(node) << "\n\n";
+	ListShow(node);
+
+	std::cout << "\nList destroy\n";
+	StringListDestroy(&node);
+	std::cout << "List size =  " << StringListSize(node) << "\n\n";
+	ListShow(node);
+	
+	return 0;
+}
